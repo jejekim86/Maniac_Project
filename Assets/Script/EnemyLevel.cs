a8738e198246e2fbdb59718a8b73ff1a43674445
YeonJangL <lej32552002@gmail.com>
2024-05-29 15:11:15 +0900

    Timer 기능 구현

---
+++ Assets/Script/EnemyLevel.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLevel : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+             
+        }
+    }
+}
